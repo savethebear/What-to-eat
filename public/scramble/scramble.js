136c754bdf11ea3856f9ba2f25295a13ddf2ca5b
savethebear <christopherlee229@gmail.com>
2019-06-07 22:24:41 +0800

    restructured project

--- public/scramble/scramble.js
+++ public/scramble/scramble.js
@@ -1,9 +1,13 @@
 (function () {
     const nav_bar_container = $(".navbar_container");
-    nav_bar_container.load("/nav.html", function() {
+    if (nav_bar_container.children().length === 0) {
+        nav_bar_container.load("/nav.html", switchCurrentTab);
+    } else {
+        switchCurrentTab();
+    }
+    
+    function switchCurrentTab() {
         nav_bar_container.find(".nav-item.active").removeClass("active");
         nav_bar_container.find(".nav-item .nav-link[page='scramble']").addClass("active");
-    });
-    
-    
+    }
 })();
