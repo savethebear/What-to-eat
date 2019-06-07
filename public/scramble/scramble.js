ec3ea3d245dc0c5d13769875be8ae9483744083b
savethebear <christopherlee229@gmail.com>
2019-06-07 22:57:21 +0800

    js to load resturants

--- public/scramble/scramble.js
+++ public/scramble/scramble.js
@@ -5,9 +5,19 @@
         nav_bar_container.append(getNavHtml());
     }
     switchCurrentTab();
+    fillResturantList();
     
     function switchCurrentTab() {
         nav_bar_container.find(".nav-item.active").removeClass("active");
         nav_bar_container.find(".nav-item .nav-link[page='scramble']").addClass("active");
+    }
+    
+    function fillResturantList() {
+        let resturants = ["Sun Lok Yun", "Thai Place", "Wildfire", "Vietnemese Place"];
+        const container = $(".resturant_list_container .resturant_list");
+        resturants.forEach(function(item) {
+            const resturant_item = '<div class="resturant_item">' + item +'<div class="delete_item"><div class="icon"></div></div></div>';
+            container.append(resturant_item);
+        });
     }
 })();
